152400a7660ecc8e101e0eeb92714860000585ff
cnoonan612 <cnoonan612@gmail.com>
2022-11-19 14:55:09 -0500

    Added link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -55,21 +55,26 @@ function updateTime() {
 }
 
 function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
+  function displayNewTime() {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
   <div class="city">
     <div>
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
     </div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-    "A"
-  )}</small></div>
+      "A"
+    )}</small></div>
   </div>
+  <a href="index.html">View all cities</a>
   `;
+  }
+  displayNewTime();
+  setInterval(displayNewTime, 1000);
 }
 
 updateTime();
